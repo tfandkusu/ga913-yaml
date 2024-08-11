117d512868843cf08d1e0dc09111468cc25dccd2
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-08-11 16:41:45 +0900

    コマンドライン引数対応

--- src/main/kotlin/com/tfandkusu/ga913yaml/Main.kt
+++ src/main/kotlin/com/tfandkusu/ga913yaml/Main.kt
@@ -1,5 +1,14 @@
 package com.tfandkusu.ga913yaml
 
-fun main() {
-    println("Hello World!")
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        println("./gradlew run validate or make")
+        return
+    }
+    val command = args[0]
+    when(command) {
+        "validate" -> println("Validate")
+        "make" -> println("Make")
+        else -> println("Unknown command")
+    }
 }
