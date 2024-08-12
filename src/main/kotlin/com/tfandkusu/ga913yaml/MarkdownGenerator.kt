9fa27d29e3bf1eefc309afd0c45b2be8c7975879
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-08-13 00:41:13 +0900

    screens.md を自動作成

---
+++ src/main/kotlin/com/tfandkusu/ga913yaml/MarkdownGenerator.kt
@@ -0,0 +1,24 @@
+package com.tfandkusu.ga913yaml
+
+import com.tfandkusu.ga913yaml.model.Screen
+import java.io.File
+
+object MarkdownGenerator {
+    fun generate(screens: List<Screen>) {
+        val builder = StringBuilder()
+        builder.append("# 画面遷移 Analytics イベント一覧\n\n")
+        builder.append("| 画面名 | Analytics イベント名 | コンバージョンイベント |\n")
+        builder.append("| -- | -- | -- |\n")
+        for (screen in screens) {
+            builder.append("| ${screen.description} | ${screen.eventName} | ${screen.isConversionEvent.toCircle()} |\n")
+        }
+        File("screens.md").writeText(builder.toString())
+    }
+
+    private fun Boolean.toCircle(): String =
+        if (this) {
+            "○"
+        } else {
+            ""
+        }
+}
