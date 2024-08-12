67a60b754d1674ea440d47df825e225104f6248e
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-08-12 23:41:51 +0900

    イベント種類数を別関数にする

--- src/main/kotlin/com/tfandkusu/ga913yaml/Validator.kt
+++ src/main/kotlin/com/tfandkusu/ga913yaml/Validator.kt
@@ -5,16 +5,16 @@ import com.tfandkusu.ga913yaml.model.Screen
 object Validator {
     fun validate(screens: List<Screen>) {
         checkEventNameLength(screens)
+        checkEventCount(screens)
     }
 
+    /**
+     * イベント名の長さが40文字を超えていないかチェックする。
+     */
     private fun checkEventNameLength(screens: List<Screen>) {
-        var eventCount = 0
         for (screen in screens) {
             if (screen.eventName.length > 40) {
                 throw IllegalArgumentException("画面遷移イベント " + screen.className + " は40文字を超えています。")
-            }
-            if (screen.isConversionEvent) {
-                eventCount++
             }
             for (action in screen.actions) {
                 if (screen.eventName.length + action.eventName.length > 40) {
@@ -22,11 +22,17 @@ object Validator {
                         "画面内操作イベント " + screen.className + "." + action.className + " は40文字を超えています。",
                     )
                 }
-                if (action.isConversionEvent) {
-                    eventCount++
-                }
             }
         }
+    }
+
+    /**
+     * イベント種類数が500を超えていないかチェックする。
+     */
+    private fun checkEventCount(screens: List<Screen>) {
+        val eventCount =
+            screens.count { it.isConversionEvent } +
+                screens.flatMap { it.actions }.count { it.isConversionEvent }
         if (eventCount > 500) {
             throw IllegalArgumentException("イベント種類数が500を超えています。")
         }
