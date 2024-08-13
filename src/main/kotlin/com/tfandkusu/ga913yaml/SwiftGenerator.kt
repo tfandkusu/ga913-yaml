b1668954a78dff24c3e3db4ae7d2594ebfe79175
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-08-14 00:19:13 +0900

    画面内操作イベント protocol の作成完了

--- src/main/kotlin/com/tfandkusu/ga913yaml/SwiftGenerator.kt
+++ src/main/kotlin/com/tfandkusu/ga913yaml/SwiftGenerator.kt
@@ -1,16 +1,20 @@
 package com.tfandkusu.ga913yaml
 
 import com.tfandkusu.ga913yaml.model.Screen
+import io.outfoxx.swiftpoet.ANY
 import io.outfoxx.swiftpoet.BOOL
+import io.outfoxx.swiftpoet.DICTIONARY
 import io.outfoxx.swiftpoet.DeclaredTypeName
 import io.outfoxx.swiftpoet.FileSpec
 import io.outfoxx.swiftpoet.PropertySpec
 import io.outfoxx.swiftpoet.STRING
 import io.outfoxx.swiftpoet.TypeSpec
+import io.outfoxx.swiftpoet.parameterizedBy
 
 object SwiftGenerator {
     private const val ROOT_STRUCT = "AnalyticsEvent"
     private val SCREEN_PROTOCOL = DeclaredTypeName.typeName(".AnalyticsEventScreen")
+    private val ACTION_PROTOCOL = DeclaredTypeName.typeName(".AnalyticsEventAction")
 
     fun generate(screens: List<Screen>) {
         FileSpec
@@ -18,6 +22,8 @@ object SwiftGenerator {
             .addComment("https://github.com/tfandkusu/ga913-yaml/ による自動生成コードです。編集しないでください。")
             .addType(
                 generateAnalyticsEventScreenProtocol(),
+            ).addType(
+                generateAnalyticsEventActionProtocol(),
             ).addType(
                 TypeSpec
                     .structBuilder(ROOT_STRUCT)
@@ -37,6 +43,30 @@ object SwiftGenerator {
                     .abstractBuilder("screenName", STRING)
                     .abstractGetter()
                     .addDoc("Analytics イベント名")
+                    .build(),
+            ).addProperty(
+                PropertySpec
+                    .abstractBuilder("isConversionEvent", BOOL)
+                    .abstractGetter()
+                    .addDoc("コンバージョンイベントフラグ")
+                    .build(),
+            ).build()
+
+    private fun generateAnalyticsEventActionProtocol(): TypeSpec =
+        TypeSpec
+            .protocolBuilder(ACTION_PROTOCOL)
+            .addDoc("画面内操作イベントのプロトコル")
+            .addProperty(
+                PropertySpec
+                    .abstractBuilder("screenName", STRING)
+                    .abstractGetter()
+                    .addDoc("Analytics イベント名")
+                    .build(),
+            ).addProperty(
+                PropertySpec
+                    .abstractBuilder("eventParameters", DICTIONARY.parameterizedBy(STRING, ANY))
+                    .abstractGetter()
+                    .addDoc("Analytics イベントパラメータ")
                     .build(),
             ).addProperty(
                 PropertySpec
